57fb134a84c1c2d2beb1a1b1b1b47388c5655e43
moisesmcardona <moises@moises-studios.com>
2018-06-19 19:53:40 -0400

    Makes the English Language Radiobutton checked by default

--- divANS_gui/My Project/Settings.Designer.vb
+++ divANS_gui/My Project/Settings.Designer.vb
@@ -56,7 +56,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
         Public Property EnglishLanguage() As Boolean
             Get
                 Return CType(Me("EnglishLanguage"),Boolean)
