3d54b9b7a402a1e62076741dec75055f7d471acb
moisesmcardona <moises@moises-studios.com>
2018-06-19 19:27:02 -0400

    Add project files.

---
+++ divANS_gui/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ActionGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ExtractRButton = New System.Windows.Forms.RadioButton()
+        Me.CompressRButton = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.SpanishRButton = New System.Windows.Forms.RadioButton()
+        Me.EnglishRButton = New System.Windows.Forms.RadioButton()
+        Me.InputFileMessage = New System.Windows.Forms.Label()
+        Me.InputFileTxt = New System.Windows.Forms.TextBox()
+        Me.BrowseFileButton = New System.Windows.Forms.Button()
+        Me.BrowseFolderButton = New System.Windows.Forms.Button()
+        Me.BrowseFile2Button = New System.Windows.Forms.Button()
+        Me.OutputFileTxt = New System.Windows.Forms.TextBox()
+        Me.OutputFileMessage = New System.Windows.Forms.Label()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.AboutLabel = New System.Windows.Forms.Label()
+        Me.VersionLabel = New System.Windows.Forms.Label()
+        Me.ActionGroupBox.SuspendLayout
+        Me.GroupBox2.SuspendLayout
+        Me.SuspendLayout
+        '
+        'ActionGroupBox
+        '
+        Me.ActionGroupBox.Controls.Add(Me.ExtractRButton)
+        Me.ActionGroupBox.Controls.Add(Me.CompressRButton)
+        Me.ActionGroupBox.Location = New System.Drawing.Point(12, 12)
+        Me.ActionGroupBox.Name = "ActionGroupBox"
+        Me.ActionGroupBox.Size = New System.Drawing.Size(152, 52)
+        Me.ActionGroupBox.TabIndex = 0
+        Me.ActionGroupBox.TabStop = false
+        Me.ActionGroupBox.Text = "Action"
+        '
+        'ExtractRButton
+        '
+        Me.ExtractRButton.AutoSize = true
+        Me.ExtractRButton.Location = New System.Drawing.Point(83, 19)
+        Me.ExtractRButton.Name = "ExtractRButton"
+        Me.ExtractRButton.Size = New System.Drawing.Size(58, 17)
+        Me.ExtractRButton.TabIndex = 1
+        Me.ExtractRButton.TabStop = true
+        Me.ExtractRButton.Text = "Extract"
+        Me.ExtractRButton.UseVisualStyleBackColor = true
+        '
+        'CompressRButton
+        '
+        Me.CompressRButton.AutoSize = true
+        Me.CompressRButton.Location = New System.Drawing.Point(6, 19)
+        Me.CompressRButton.Name = "CompressRButton"
+        Me.CompressRButton.Size = New System.Drawing.Size(71, 17)
+        Me.CompressRButton.TabIndex = 0
+        Me.CompressRButton.TabStop = true
+        Me.CompressRButton.Text = "Compress"
+        Me.CompressRButton.UseVisualStyleBackColor = true
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.SpanishRButton)
+        Me.GroupBox2.Controls.Add(Me.EnglishRButton)
+        Me.GroupBox2.Location = New System.Drawing.Point(170, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(152, 52)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = false
+        Me.GroupBox2.Text = "Language / Idioma"
+        '
+        'SpanishRButton
+        '
+        Me.SpanishRButton.AutoSize = true
+        Me.SpanishRButton.Location = New System.Drawing.Point(71, 19)
+        Me.SpanishRButton.Name = "SpanishRButton"
+        Me.SpanishRButton.Size = New System.Drawing.Size(63, 17)
+        Me.SpanishRButton.TabIndex = 1
+        Me.SpanishRButton.TabStop = true
+        Me.SpanishRButton.Text = "Español"
+        Me.SpanishRButton.UseVisualStyleBackColor = true
+        '
+        'EnglishRButton
+        '
+        Me.EnglishRButton.AutoSize = true
+        Me.EnglishRButton.Location = New System.Drawing.Point(6, 19)
+        Me.EnglishRButton.Name = "EnglishRButton"
+        Me.EnglishRButton.Size = New System.Drawing.Size(59, 17)
+        Me.EnglishRButton.TabIndex = 0
+        Me.EnglishRButton.TabStop = true
+        Me.EnglishRButton.Text = "English"
+        Me.EnglishRButton.UseVisualStyleBackColor = true
+        '
+        'InputFileMessage
+        '
+        Me.InputFileMessage.AutoSize = true
+        Me.InputFileMessage.Location = New System.Drawing.Point(9, 77)
+        Me.InputFileMessage.Name = "InputFileMessage"
+        Me.InputFileMessage.Size = New System.Drawing.Size(223, 13)
+        Me.InputFileMessage.TabIndex = 3
+        Me.InputFileMessage.Text = "Step 1: Browse for a file or folder to compress:"
+        '
+        'InputFileTxt
+        '
+        Me.InputFileTxt.Location = New System.Drawing.Point(12, 93)
+        Me.InputFileTxt.Name = "InputFileTxt"
+        Me.InputFileTxt.Size = New System.Drawing.Size(198, 20)
+        Me.InputFileTxt.TabIndex = 4
+        '
+        'BrowseFileButton
+        '
+        Me.BrowseFileButton.Location = New System.Drawing.Point(216, 91)
+        Me.BrowseFileButton.Name = "BrowseFileButton"
+        Me.BrowseFileButton.Size = New System.Drawing.Size(75, 23)
+        Me.BrowseFileButton.TabIndex = 5
+        Me.BrowseFileButton.Text = "Browse File"
+        Me.BrowseFileButton.UseVisualStyleBackColor = true
+        '
+        'BrowseFolderButton
+        '
+        Me.BrowseFolderButton.Location = New System.Drawing.Point(297, 91)
+        Me.BrowseFolderButton.Name = "BrowseFolderButton"
+        Me.BrowseFolderButton.Size = New System.Drawing.Size(75, 23)
+        Me.BrowseFolderButton.TabIndex = 6
+        Me.BrowseFolderButton.Text = "Browse Folder"
+        Me.BrowseFolderButton.UseVisualStyleBackColor = true
+        '
+        'BrowseFile2Button
+        '
+        Me.BrowseFile2Button.Location = New System.Drawing.Point(216, 140)
+        Me.BrowseFile2Button.Name = "BrowseFile2Button"
+        Me.BrowseFile2Button.Size = New System.Drawing.Size(75, 23)
+        Me.BrowseFile2Button.TabIndex = 9
+        Me.BrowseFile2Button.Text = "Browse File"
+        Me.BrowseFile2Button.UseVisualStyleBackColor = true
+        '
+        'OutputFileTxt
+        '
+        Me.OutputFileTxt.Location = New System.Drawing.Point(12, 142)
+        Me.OutputFileTxt.Name = "OutputFileTxt"
+        Me.OutputFileTxt.Size = New System.Drawing.Size(198, 20)
+        Me.OutputFileTxt.TabIndex = 8
+        '
+        'OutputFileMessage
+        '
+        Me.OutputFileMessage.AutoSize = true
+        Me.OutputFileMessage.Location = New System.Drawing.Point(9, 126)
+        Me.OutputFileMessage.Name = "OutputFileMessage"
+        Me.OutputFileMessage.Size = New System.Drawing.Size(278, 13)
+        Me.OutputFileMessage.TabIndex = 7
+        Me.OutputFileMessage.Text = "Step 2: Browse for a location to save the compressed file:"
+        '
+        'StartButton
+        '
+        Me.StartButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 24!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.StartButton.Location = New System.Drawing.Point(12, 178)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(360, 40)
+        Me.StartButton.TabIndex = 10
+        Me.StartButton.Text = "Start!"
+        Me.StartButton.UseVisualStyleBackColor = true
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'AboutLabel
+        '
+        Me.AboutLabel.AutoSize = true
+        Me.AboutLabel.Location = New System.Drawing.Point(12, 225)
+        Me.AboutLabel.Name = "AboutLabel"
+        Me.AboutLabel.Size = New System.Drawing.Size(165, 13)
+        Me.AboutLabel.TabIndex = 11
+        Me.AboutLabel.Text = "GUI software by: Moisés Cardona"
+        '
+        'VersionLabel
+        '
+        Me.VersionLabel.AutoSize = true
+        Me.VersionLabel.Location = New System.Drawing.Point(344, 225)
+        Me.VersionLabel.Name = "VersionLabel"
+        Me.VersionLabel.Size = New System.Drawing.Size(28, 13)
+        Me.VersionLabel.TabIndex = 12
+        Me.VersionLabel.Text = "v1.0"
+        '
+        'Form1
+        '
+        Me.AllowDrop = true
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(386, 247)
+        Me.Controls.Add(Me.VersionLabel)
+        Me.Controls.Add(Me.AboutLabel)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.BrowseFile2Button)
+        Me.Controls.Add(Me.OutputFileTxt)
+        Me.Controls.Add(Me.OutputFileMessage)
+        Me.Controls.Add(Me.BrowseFolderButton)
+        Me.Controls.Add(Me.BrowseFileButton)
+        Me.Controls.Add(Me.InputFileTxt)
+        Me.Controls.Add(Me.InputFileMessage)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.ActionGroupBox)
+        Me.MaximizeBox = false
+        Me.Name = "Form1"
+        Me.Text = "divANS Graphical User Interface"
+        Me.ActionGroupBox.ResumeLayout(false)
+        Me.ActionGroupBox.PerformLayout
+        Me.GroupBox2.ResumeLayout(false)
+        Me.GroupBox2.PerformLayout
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents ActionGroupBox As GroupBox
+    Friend WithEvents ExtractRButton As RadioButton
+    Friend WithEvents CompressRButton As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents SpanishRButton As RadioButton
+    Friend WithEvents EnglishRButton As RadioButton
+    Friend WithEvents InputFileMessage As Label
+    Friend WithEvents InputFileTxt As TextBox
+    Friend WithEvents BrowseFileButton As Button
+    Friend WithEvents BrowseFolderButton As Button
+    Friend WithEvents BrowseFile2Button As Button
+    Friend WithEvents OutputFileTxt As TextBox
+    Friend WithEvents OutputFileMessage As Label
+    Friend WithEvents StartButton As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents AboutLabel As Label
+    Friend WithEvents VersionLabel As Label
+End Class
---
+++ divANS_gui/Form1.vb
@@ -0,0 +1,294 @@
+﻿Public Class Form1
+    Private OutputFileName As String = String.Empty
+    Private divans_version As String = "001"
+    Private Running As Boolean = False
+
+    Private AboutString As String = My.Resources.About
+    Private ActionGroupBoxString As String = My.Resources.Action
+    Private BrowseFileButtonString As String = My.Resources.BrowseFileButton
+    Private BrowseFolderButtonString As String = My.Resources.BrowseFolderButton
+    Private BrowseOutputButton As String = My.Resources.BrowseOutputButton
+    Private CompressFolderSelectedMessage As String = My.Resources.CompressFolderSelectedMessage
+    Private CompressInputMessage As String = My.Resources.CompressInputMessage
+    Private CompressOutputMessage As String = My.Resources.CompressOutputMessage
+    Private CompressRButtonString As String = My.Resources.CompressRButton
+    Private CompressingString As String = My.Resources.Compressing
+    Private ExtractFolderSelectedMessage As String = My.Resources.ExtractFolderSelectedMessage
+    Private ExtractInputMessage As String = My.Resources.ExtractInputMessage
+    Private ExtractOutputMessage As String = My.Resources.ExtractOutputMessage
+    Private ExtractRButtonString As String = My.Resources.ExtractRButton
+    Private ExtractingString As String = My.Resources.Extracting
+    Private StartButtonString As String = My.Resources.StartButton
+    Private Finished As String = My.Resources.Finished
+    Private FormName As String = My.Resources.FormName
+
+    Private Sub SetOutputFilename()
+        If CompressRButton.Checked Then OutputFileTxt.Text = OutputFileName + divans_version
+    End Sub
+    Private Sub SetOutputFileNamePathWithoutExtension(Path As String)
+        OutputFileTxt.Text = My.Computer.FileSystem.GetParentPath(Path) + "\" + IO.Path.GetFileNameWithoutExtension(Path)
+    End Sub
+    Private Function GetInputNameAndUpdateForm(Path As String) As String
+        Dim CheckIfFile = CheckIfFileOrFolder(Path)
+        If CheckIfFile = "File" Then
+            Dim FileExtension As String = IO.Path.GetExtension(Path)
+            If FileExtension.Contains("divans") Then
+                SetOutputFileNamePathWithoutExtension(Path)
+                ExtractRButton.Checked = True
+            End If
+            If CompressRButton.Checked Then
+                OutputFileName = Path + ".divans"
+                SetOutputFilename()
+            End If
+        End If
+        Return Path
+    End Function
+    Private Sub UpdateElementsInForm()
+        If CompressRButton.Checked Then
+            InputFileMessage.Text = CompressInputMessage
+        ElseIf ExtractRButton.Checked Then
+            InputFileMessage.Text = ExtractInputMessage
+        End If
+        AboutLabel.Text = AboutString
+        ActionGroupBox.Text = ActionGroupBoxString
+        BrowseFileButton.Text = BrowseFileButtonString
+        BrowseFolderButton.Text = BrowseFolderButtonString
+        BrowseFile2Button.Text = BrowseOutputButton
+        CompressRButton.Text = CompressRButtonString
+        ExtractRButton.Text = ExtractRButtonString
+        StartButton.Text = StartButtonString
+        If Running Then UpdateStartButton()
+        Me.Text = FormName
+    End Sub
+    Private Sub UpdateMessageStrings(Language As String)
+        If Language = "English" Then
+            AboutString = My.Resources.About
+            ActionGroupBoxString = My.Resources.Action
+            BrowseFileButtonString = My.Resources.BrowseFileButton
+            BrowseFolderButtonString = My.Resources.BrowseFolderButton
+            BrowseOutputButton = My.Resources.BrowseOutputButton
+            CompressFolderSelectedMessage = My.Resources.CompressFolderSelectedMessage
+            CompressInputMessage = My.Resources.CompressInputMessage
+            CompressOutputMessage = My.Resources.CompressOutputMessage
+            CompressRButtonString = My.Resources.CompressRButton
+            CompressingString = My.Resources.Compressing
+            ExtractFolderSelectedMessage = My.Resources.ExtractFolderSelectedMessage
+            ExtractInputMessage = My.Resources.ExtractInputMessage
+            ExtractOutputMessage = My.Resources.ExtractOutputMessage
+            ExtractRButtonString = My.Resources.ExtractRButton
+            ExtractingString = My.Resources.Extracting
+            StartButtonString = My.Resources.StartButton
+            Finished = My.Resources.Finished
+            FormName = My.Resources.FormName
+        Else
+            AboutString = My.Resources.AboutSpanish
+            ActionGroupBoxString = My.Resources.ActionSpanish
+            BrowseFileButtonString = My.Resources.BrowseFileButtonSpanish
+            BrowseFolderButtonString = My.Resources.BrowseFolderButtonSpanish
+            BrowseOutputButton = My.Resources.BrowseOutputButtonSpanish
+            CompressFolderSelectedMessage = My.Resources.CompressFolderSelectedMessageSpanish
+            CompressInputMessage = My.Resources.CompressInputMessageSpanish
+            CompressOutputMessage = My.Resources.CompressOutputMessageSpanish
+            CompressRButtonString = My.Resources.CompressRButtonSpanish
+            CompressingString = My.Resources.CompressingSpanish
+            ExtractFolderSelectedMessage = My.Resources.ExtractFolderSelectedMessageSpanish
+            ExtractInputMessage = My.Resources.ExtractInputMessageSpanish
+            ExtractOutputMessage = My.Resources.ExtractOutputMessageSpanish
+            ExtractRButtonString = My.Resources.ExtractRButtonSpanish
+            ExtractingString = My.Resources.ExtractingSpanish
+            StartButtonString = My.Resources.StartButtonSpanish
+            Finished = My.Resources.FinishedSpanish
+            FormName = My.Resources.FormNameSpanish
+        End If
+        UpdateElementsInForm()
+    End Sub
+    Private Function CheckIfFileOrFolder(Optional PathToCheck As String = "") As String
+        If My.Computer.FileSystem.FileExists(PathToCheck) Or PathToCheck = "" Then
+            If CompressRButton.Checked Then
+                OutputFileMessage.Text = CompressOutputMessage
+            ElseIf ExtractRButton.Checked Then
+                OutputFileMessage.Text = ExtractOutputMessage
+            End If
+            OutputFileTxt.Enabled = True
+            BrowseFile2Button.Enabled = True
+            If PathToCheck IsNot "" Then Return "File"
+        ElseIf My.Computer.FileSystem.DirectoryExists(PathToCheck) Then
+            If CompressRButton.Checked Then
+                OutputFileMessage.Text = CompressFolderSelectedMessage
+            ElseIf ExtractRButton.Checked Then
+                OutputFileMessage.Text = ExtractFolderSelectedMessage
+            End If
+            OutputFileTxt.Enabled = False
+            BrowseFile2Button.Enabled = False
+            Return "Folder"
+        End If
+        Return "N/A"
+    End Function
+    Private Sub InputFileTxt_TextChanged(sender As Object, e As EventArgs) Handles InputFileTxt.TextChanged
+        GetInputNameAndUpdateForm(InputFileTxt.Text)
+    End Sub
+
+    Private Sub EnglishRButton_CheckedChanged(sender As Object, e As EventArgs) Handles EnglishRButton.CheckedChanged
+        My.Settings.EnglishLanguage = EnglishRButton.Checked
+        My.Settings.Save()
+        UpdateMessageStrings("English")
+    End Sub
+
+    Private Sub SpanishRButton_CheckedChanged(sender As Object, e As EventArgs) Handles SpanishRButton.CheckedChanged
+        My.Settings.SpanishLanguage = SpanishRButton.Checked
+        My.Settings.Save()
+        UpdateMessageStrings("Spanish")
+    End Sub
+
+    Private Sub CompressRButton_CheckedChanged(sender As Object, e As EventArgs) Handles CompressRButton.CheckedChanged
+        InputFileMessage.Text = CompressInputMessage
+        OutputFileMessage.Text = CompressOutputMessage
+        My.Settings.Compress = CompressRButton.Checked
+        My.Settings.Save()
+        If InputFileTxt.Text IsNot String.Empty Then GetInputNameAndUpdateForm(InputFileTxt.Text)
+    End Sub
+
+    Private Sub ExtractRButton_CheckedChanged(sender As Object, e As EventArgs) Handles ExtractRButton.CheckedChanged
+        InputFileMessage.Text = ExtractInputMessage
+        OutputFileMessage.Text = ExtractOutputMessage
+        My.Settings.Extract = ExtractRButton.Checked
+        My.Settings.Save()
+        If InputFileTxt.Text IsNot String.Empty Then GetInputNameAndUpdateForm(InputFileTxt.Text)
+    End Sub
+
+    Private Sub BrowseFileButton_Click(sender As Object, e As EventArgs) Handles BrowseFileButton.Click
+        OpenFileDialog1.Title = InputFileMessage.Text
+        OpenFileDialog1.Filter = "All files (*.*)|*.*"
+        If InputFileTxt.Text IsNot String.Empty Then
+            If My.Computer.FileSystem.FileExists(InputFileTxt.Text) Then OpenFileDialog1.FileName = My.Computer.FileSystem.GetName(InputFileTxt.Text) Else OpenFileDialog1.FileName = String.Empty
+        Else
+            OpenFileDialog1.FileName = String.Empty
+        End If
+        Dim response As DialogResult = OpenFileDialog1.ShowDialog
+        If response = DialogResult.OK Then
+            InputFileTxt.Text = GetInputNameAndUpdateForm(OpenFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub BrowseFolderButton_Click(sender As Object, e As EventArgs) Handles BrowseFolderButton.Click
+        Dim response As DialogResult = FolderBrowserDialog1.ShowDialog()
+        If response = DialogResult.OK Then
+            InputFileTxt.Text = GetInputNameAndUpdateForm(FolderBrowserDialog1.SelectedPath)
+        End If
+    End Sub
+
+    Private Sub BrowseFile2Button_Click(sender As Object, e As EventArgs) Handles BrowseFile2Button.Click
+        SaveFileDialog1.Title = OutputFileMessage.Text
+        If CompressRButton.Checked Then SaveFileDialog1.Filter = "divans file|*.divans" Else SaveFileDialog1.Filter = "All files (*.*)|*.*"
+        If OutputFileTxt.Text IsNot String.Empty Then
+            If My.Computer.FileSystem.FileExists(OutputFileTxt.Text) Then SaveFileDialog1.FileName = My.Computer.FileSystem.GetName(OutputFileTxt.Text) Else SaveFileDialog1.FileName = String.Empty
+        Else
+            SaveFileDialog1.FileName = String.Empty
+        End If
+        Dim response As DialogResult = SaveFileDialog1.ShowDialog
+        If response = DialogResult.OK Then
+            If CompressRButton.Checked Then
+                OutputFileName = SaveFileDialog1.FileName
+                If CompressRButton.Checked Then SetOutputFilename()
+            Else
+                OutputFileTxt.Text = SaveFileDialog1.FileName
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles MyBase.DragDrop
+        InputFileTxt.Text = e.Data.GetData(DataFormats.FileDrop)(0)
+        GetInputNameAndUpdateForm(InputFileTxt.Text)
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles MyBase.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+    Public Sub ProcessFiles(Folder As String, Action As String)
+        For Each File In IO.Directory.GetFiles(Folder)
+            If ExtractRButton.Checked Then
+                If IO.Path.GetExtension(File).Contains("cmix") Then
+                    GetInputNameAndUpdateForm(File)
+                    Run_divans(File, OutputFileTxt.Text, Action)
+                End If
+            Else
+                Run_divans(File, File + ".divans" + divans_version, Action)
+            End If
+        Next
+    End Sub
+
+    Public Sub ProcessSubfolders(Folder As String, Action As String)
+        For Each Subfolder In IO.Directory.GetDirectories(Folder)
+            ProcessFolder(Subfolder, Action)
+        Next
+    End Sub
+
+    Public Sub ProcessFolder(Folder As String, Action As String)
+        ProcessFiles(Folder, Action)
+        ProcessSubfolders(Folder, Action)
+    End Sub
+    Private Delegate Sub UpdateStartButtonInvoker(Finished As Boolean)
+    Private Sub UpdateStartButton(Optional Finished As Boolean = False)
+        If StartButton.InvokeRequired Then
+            StartButton.Invoke(New UpdateStartButtonInvoker(AddressOf UpdateStartButton), Finished)
+        Else
+            If Finished Then
+                StartButton.Enabled = True
+                StartButton.Text = StartButtonString
+            Else
+                StartButton.Enabled = False
+                If CompressRButton.Checked Then
+                    StartButton.Text = CompressingString
+                Else
+                    StartButton.Text = ExtractingString
+                End If
+            End If
+        End If
+    End Sub
+    Private Sub ProcessThread()
+        Running = True
+        Dim ProcessAction As String = ""
+        If CompressRButton.Checked Then
+            ProcessAction = "-c"
+        End If
+        UpdateStartButton()
+        Dim CheckInput As String = CheckIfFileOrFolder(InputFileTxt.Text)
+        If CheckInput = "File" Then
+            If OutputFileTxt.Text IsNot String.Empty Then
+                Run_divans(InputFileTxt.Text, OutputFileTxt.Text, ProcessAction)
+            End If
+        ElseIf CheckInput = "Folder" Then
+            ProcessFolder(InputFileTxt.Text, ProcessAction)
+        End If
+        UpdateStartButton(True)
+        Running = False
+        MessageBox.Show(Finished)
+    End Sub
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        If InputFileTxt.Text IsNot String.Empty Then
+            Dim Thread As New Threading.Thread(Sub() ProcessThread())
+            Thread.Start()
+        End If
+    End Sub
+    Private Sub Run_divans(Input As String, Output As String, action As String)
+        Dim cmixProcessInfo As New ProcessStartInfo
+        Dim cmixProcess As Process
+        cmixProcessInfo.FileName = My.Settings.Version + ".exe"
+        cmixProcessInfo.Arguments = action + " """ + Input + """ """ + Output + """"
+        cmixProcessInfo.CreateNoWindow = True
+        cmixProcessInfo.RedirectStandardOutput = True
+        cmixProcessInfo.UseShellExecute = False
+        cmixProcess = Process.Start(cmixProcessInfo)
+        Dim currentOutput As String = String.Empty
+        cmixProcess.WaitForExit()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CompressRButton.Checked = My.Settings.Compress
+        ExtractRButton.Checked = My.Settings.Extract
+        EnglishRButton.Checked = My.Settings.EnglishLanguage
+        SpanishRButton.Checked = My.Settings.SpanishLanguage
+    End Sub
+End Class
